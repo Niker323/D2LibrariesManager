f45d56c770eb148466bdaf5ec6e5bd3746d91ed6
Niker323 <artiom123.budko@yandex.by>
2021-03-26 10:18:17 +0300

    Initial commit

---
+++ D2LibraryManager/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace D2LibraryManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 60;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 18;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 26);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer2.Panel2.Controls.Add(this.menuStrip1);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 386);
+            this.splitContainer2.SplitterDistance = 266;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.treeView1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(262, 382);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowWebBrowserDrop = false;
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 24);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(526, 358);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(526, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "D2LibraryManager";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ D2LibraryManager/Form1.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D2LibraryManager
+{
+    public partial class Form1 : Form
+    {
+        string steamPath;
+        string addonName;
+        string addonGamePath
+        {
+            get
+            {
+                return steamPath + "\\steamapps\\common\\dota 2 beta\\game\\dota_addons\\" + addonName;
+            }
+        }
+        string addonContentPath
+        {
+            get
+            {
+                return steamPath + "\\steamapps\\common\\dota 2 beta\\content\\dota_addons\\" + addonName;
+            }
+        }
+
+        List<string> addonList = new List<string>();
+        List<LibData> libList = new List<LibData>();
+
+        string discr_blank = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            string keypath = @"Software\Valve\Steam";
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(keypath);
+            steamPath = key.GetValue("SteamPath").ToString();
+            List<string> addon_list = Directory.GetDirectories(steamPath + "\\steamapps\\common\\dota 2 beta\\game\\dota_addons").ToList();
+            for (int i = 0; i < addon_list.Count; i++)
+            {
+                if (!File.Exists(addon_list[i] + "\\readonly_tools_asset_info.bin"))
+                {
+                    addonList.Add(addon_list[i].Replace(steamPath + "\\steamapps\\common\\dota 2 beta\\game\\dota_addons\\", ""));
+                    comboBox1.Items.Add(addonList.Last<string>());
+                }
+            }
+            if (File.Exists(steamPath + "\\steamapps\\common\\dota 2 beta\\game\\dota_addons\\dota2cfg.cfg"))
+            {
+                string def_str = File.ReadAllText(steamPath + "\\steamapps\\common\\dota 2 beta\\game\\dota_addons\\dota2cfg.cfg");
+                def_str = def_str.Substring(def_str.IndexOf("default"));
+                def_str = def_str.Substring(def_str.IndexOf('"') + 1);
+                def_str = def_str.Substring(def_str.IndexOf('"') + 1);
+                addonName = def_str.Substring(0, def_str.IndexOf('"'));
+                if (!Directory.Exists(addonGamePath)) addonName = addonList.First();
+            }
+            else
+            {
+                addonName = addonList.First();
+            }
+            comboBox1.Text = addonName;
+            string libs_dir = @"Libs";
+            if (Directory.Exists(libs_dir))
+            {
+                List<string> libs_list = Directory.GetDirectories(libs_dir).ToList();
+                for (int i = 0; i < libs_list.Count; i++)
+                {
+                    if (File.Exists(libs_list[i] + "\\data.json"))
+                    {
+                        string lib_json = File.ReadAllText(libs_list[i] + "\\data.json");
+                        JObject jObject = JObject.Parse(lib_json);
+                        libList.Add(jObject.ToObject<LibData>());
+                    }
+                    if (File.Exists(libs_list[i] + "\\description.html"))
+                    {
+                        libList.Last().description = File.ReadAllText(libs_list[i] + "\\description.html");
+                    }
+                    if (File.Exists(libs_list[i] + "\\install.txt"))
+                    {
+                        libList.Last().install = File.ReadLines(libs_list[i] + "\\install.txt").ToArray();
+                    }
+                    libList.Last().path = libs_list[i];
+                }
+            }
+            for (int i = 0; i < libList.Count; i++)
+            {
+                if (libList[i].parent == null)
+                {
+                    CreateNodeAndParents(libList[i], treeView1.Nodes);
+                }
+            }
+            if (File.Exists(@"blank.html"))
+            {
+                discr_blank = File.ReadAllText(@"blank.html");
+            }
+        }
+
+        private void CreateNodeAndParents(LibData lib, TreeNodeCollection parent)
+        {
+            TreeNode newNode = parent.Add(lib.name);
+            newNode.Tag = lib;
+            for (int i = 0; i < libList.Count; i++)
+            {
+                if (libList[i].parent != null)
+                {
+                    if (lib.name == libList[i].parent)
+                    {
+                        CreateNodeAndParents(libList[i], newNode.Nodes);
+                    }
+                }
+            }
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            addonName = comboBox1.Text;
+            UpdateNodes(treeView1.Nodes);
+        }
+
+        private void UpdateNodes(TreeNodeCollection nodes)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                UpdateNode(nodes[i]);
+                UpdateNodes(nodes[i].Nodes);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            UpdateNode(treeView1.SelectedNode);
+        }
+
+        private void UpdateNode(TreeNode node)
+        {
+            LibData selectedLibData = (LibData)node.Tag;
+            string ready_discr = discr_blank.Replace("{name}", selectedLibData.name);
+            ready_discr = ready_discr.Replace("{source}", selectedLibData.source);
+            ready_discr = ready_discr.Replace("{description}", selectedLibData.description);
+            webBrowser1.DocumentText = ready_discr;
+            menuStrip1.Items.Clear();
+            if (selectedLibData.install != null)
+            {
+                if (IsInstalled(selectedLibData))
+                {
+                    treeView1.SelectedNode.ForeColor = Color.LightGreen;
+                    ToolStripItem button = menuStrip1.Items.Add("Uninstall");
+                    button.Click += OnUninstall;
+                }
+                else
+                {
+                    treeView1.SelectedNode.ForeColor = Color.White;
+                    ToolStripItem button = menuStrip1.Items.Add("Install");
+                    button.Click += OnInstall;
+                }
+            }
+        }
+
+        private void OnInstall(object sender, EventArgs e)
+        {
+            LibData selectedLibData = (LibData)treeView1.SelectedNode.Tag;
+            if (selectedLibData.link != null && selectedLibData.install != null)
+            {
+                if (Directory.Exists("library")) Directory.Delete("library", true);
+                if (File.Exists("library.zip")) File.Delete("library.zip");
+                new WebClient().DownloadFile(selectedLibData.link, "library.zip");
+                ZipFile.ExtractToDirectory("library.zip", "library");
+                for (int i = 0; i < selectedLibData.install.Length; i++)
+                {
+                    DoLine(selectedLibData.install[i], false);
+                }
+                if (Directory.Exists("library")) Directory.Delete("library", true);
+                if (File.Exists("library.zip")) File.Delete("library.zip");
+                UpdateNode(treeView1.SelectedNode);
+            }
+            else
+            {
+                MessageBox.Show("Install error:" + Environment.NewLine + "Link not found");
+            }
+        }
+
+        private void OnUninstall(object sender, EventArgs e)
+        {
+            LibData selectedLibData = (LibData)treeView1.SelectedNode.Tag;
+            if (selectedLibData.install != null)
+            {
+                for (int i = 0; i < selectedLibData.install.Length; i++)
+                {
+                    DoLine(selectedLibData.install[i], true);
+                }
+                UpdateNode(treeView1.SelectedNode);
+            }
+        }
+
+        private void DoLine(string str, bool reverse)
+        {
+            try
+            {
+                string[] args = str.Split('@');
+                if (reverse)
+                {
+                    if (args[0] == "addline")
+                    {
+                        File.WriteAllText(FormPath(args[1]), File.ReadAllText(FormPath(args[1])).Replace(args[2] + Environment.NewLine, ""));
+                    }
+                    else if(args[0] == "copy")
+                    {
+                        File.Delete(FormPath(args[2]));
+                    }
+                }
+                else
+                {
+                    if (args[0] == "addline")
+                    {
+                        File.WriteAllText(FormPath(args[1]), args[2] + Environment.NewLine + File.ReadAllText(FormPath(args[1])));
+                    }
+                    else if (args[0] == "copy")
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(FormPath(args[2])));
+                        File.Copy(FormPath(args[1]), FormPath(args[2]), false);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Install error:" + Environment.NewLine + str);
+            }
+        }
+
+        private string FormPath(string str)
+        {
+            return str.Replace("{game}", addonGamePath).Replace("{content}", addonContentPath);
+        }
+
+        private bool IsInstalled(LibData lib)
+        {
+            if (lib.install != null)
+            {
+                for (int i = 0; i < lib.install.Length; i++)
+                {
+                    string[] args = lib.install[i].Split('@');
+                    if (args[0] == "copy")
+                    {
+                        if (File.Exists(FormPath(args[2])))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public class LibData
+    {
+        public string name;
+        public string source;
+        public string link;
+        public string description;
+        public string path;
+        public string[] install;
+        public string parent;
+    }
+}
